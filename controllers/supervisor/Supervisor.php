760dad51fdd627cd63d2af7f81ef9a9ebeba685d
Kumud Perera <Nethmalperera54@gmail.com>
2021-09-19 01:23:42 +0530

    Supervisor Landing page added

---
+++ controllers/supervisor/Supervisor.php
@@ -0,0 +1,17 @@
+<?php
+
+class Supervisor extends Controller{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function index() {
+
+        $this->view->showPage('Supervisor/Supervisor');
+    }
+
+
+}
+
+?>
---
+++ views/supervisor/supervisor.php
@@ -0,0 +1,28 @@
+<?php include 'topContainer.php';?>
+    
+    <div class="middle-container">
+        <h1>GOOD SUPPERVISOR</h1>
+        <hr>
+        
+        <div class="img-btn-container">
+            <div class="left">
+                <img src="<?php echo URL?>vendors/images/supervisor/1.png" alt="Image-1">
+            </div>
+
+            <div class="right">
+                <img src="<?php echo URL?>vendors/images/supervisor/2.png" alt="Image-2">
+            </div>
+
+            <div class="btn-list">
+                <button class="btn btn-land"><i class="fas fa-user"></i> LAND OWNERS</button>
+
+                <button class="btn btn-fertilizer"><i class="fas fa-tree"></i> FERTILIZER</button>
+
+                <button class="btn btn-firewood"><i class="fas fa-fire"></i> FIREWOOD</button>
+
+                <button class="btn btn-request"><i class="fas fa-question"></i> REQUESTS </button>
+            </div>
+        </div>
+        <hr>
+    </div>
+<?php include 'views/bottomContainer.php';?>
