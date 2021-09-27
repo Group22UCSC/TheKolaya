2df92f9ea46cc91d52d97b9b77129ffc1ac3e3d3
SasinduDias <sasiwijegunasinghe98@gmail.com>
2021-09-27 22:46:19 +0530

    Admin dash board add

--- views/manager/manager.php
+++ views/manager/manager.php
@@ -1,6 +1,65 @@
 <?php include 'top-container.php';?>
-
+<link rel="stylesheet" href="<?php echo URL?>vendors/css/manager/manager-style.css">
 <div class="text">Dashboard</div>
+
+
+
+<div class="king">
+	
+
+
+<div class="top">
+
+<!-- https://codepen.io/jlalovi/details/bIyAr -->
+<div class="container">
+  <!-- DONUT CHART BLOCK (LEFT-CONTAINER) --> 
+  <div class="donut-chart-block block"> 
+                    <h2 class="titular">MONTHLY REPORT</h2>
+                    <div class="donut-chart">
+ <!-- PORCIONES GRAFICO CIRCULAR
+      ELIMINADO #donut-chart
+      MODIFICADO CSS de .donut-chart -->
+      <div id="porcion1" class="recorte"><div class="quesito ios" data-rel="21"></div></div>
+     <div id="porcion2" class="recorte"><div class="quesito mac" data-rel="39"></div></div>
+     <div id="porcion3" class="recorte"><div class="quesito win" data-rel="31"></div></div>
+     <div id="porcionFin" class="recorte"><div class="quesito linux" data-rel="9"></div></div>
+ <!-- FIN AÑADIDO GRÄFICO -->
+                            <p class="center-date">MARCH<br><span class="scnd-font-color">2022</span></p>        
+                    </div>
+                    <ul class="os-percentages horizontal-list">
+                        <li>
+                            <p class="ios os scnd-font-color">Landowner Payment</p>
+                            <p class="os-percentage">30<sup>%</sup></p>
+                        </li>
+                        <li>
+                            <p class="mac os scnd-font-color">Employee Payment</p>
+                            <p class="os-percentage">10<sup>%</sup></p>
+                        </li>
+                        <li>
+                            <p class="linux os scnd-font-color">Firewood Payment</p>
+                            <p class="os-percentage">10<sup>%</sup></p>
+                        </li>
+                        <li>
+                            <p class="win os scnd-font-color">Factory Profit</p>
+                            <p class="os-percentage">50<sup>%</sup></p>
+                        </li>
+                    </ul>
+                </div>
+
+</div>
+
+
+<div class="img">
+	
+	<div class="bartop">
+		SAMRIN
+	</div>
+</div>
+
+
+
+	
+</div>
 
 
 
