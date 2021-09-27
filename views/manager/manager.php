c23bdf5491cb74d2e164986e1f40eb3b7e77dd58
SasinduDias <sasiwijegunasinghe98@gmail.com>
2021-09-28 00:50:44 +0530

    Admin and manager landing pages design

--- views/manager/manager.php
+++ views/manager/manager.php
@@ -47,19 +47,26 @@
                 </div>
 
 </div>
-
+</div>
 
 <div class="img">
 	
-	<div class="bartop">
-		SAMRIN
+		<h1>Welcome to Home of Samrin Teas</h1> <br>
+  <h2>Taste of Excellence</h2> <br><br> 
+
+<div class="last">
+	It is a matter of pride for Samrin Tea Estates PLC to be a part of the Hayleys Group, one of the largest and most diversified multinational conglomerates in Sri Lanka with a history spanning 140 years, with whom we continue to bring you the greatest Ceylon teas of the land. <br>
+
+</div>
+<!-- <br> <br> <br> Our mission statement as a plantations company in Sri Lanka is simple: manage our tea plantations to enhance the quality of life of all our employees, produce and market high quality Ceylon teas that delight our customers, drive sustainable growth and enhance shareholder value. We at Samrin Tea Estates PLC know that if passion, hard work and excellence go into an endeavour, the outcome will be a quality experience, and therefore rewarding. <br> We confess we are obsessed over the quality of our teas - our Ceylon Black Tea, Ceylon Green Tea and Ceylon Speciality Tea. The results are evident in every cup of tea made in our tea gardens. -->
+
 	</div>
 </div>
 
 
 
 	
-</div>
+
 
 
 
