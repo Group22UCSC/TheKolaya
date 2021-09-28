12e93b1e50375d1f78177e2be8a2d3ef4ad8ba72
SasinduDias <sasiwijegunasinghe98@gmail.com>
2021-09-28 18:27:39 +0530

    Admin and manager land

---
+++ views/manager/viewStock.php
@@ -0,0 +1,18 @@
+
+<?php include 'top-container.php';?>
+
+ <link rel="stylesheet" href="<?php echo URL?>vendors/css/manager/viewStock.css">
+
+ <div class="first">STOCK DETAILS</div>
+
+    <div class="middle">
+        <div class="btn-list">
+          <a href="<?php echo URL?>manager/viewFertilizer"> <button class="btn btn-account"><i class="fas fa-luggage-cart"></i> FERTILIZER DETAILS</button></a>
+
+           <a href="<?php echo URL?>manager/viewFirewood"> <button class="btn btn-tea"><i class="fas fa-dumpster-fire"></i>FIREWOOD DETAILS</button></a>
+
+           <a href="<?php echo URL?>manager/viewProduct"><button class="btn btn-payments"><i class="fab fa-product-hunt"></i>PRODUCT DETAILS</button>
+        </div>
+    </div>
+    
+<?php include 'bottom-container.php';?>
