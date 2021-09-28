293fc38a78b956cacee7d84666145d80e3e0061b
Melaka36 <melaka36pathiranagama@gmail.com>
2021-09-28 20:58:58 +0530

    added

---
+++ views/admin/updateAccount1.php
@@ -0,0 +1,74 @@
+
+
+<?php include 'top-container.php';?>
+
+<link rel="stylesheet" href="<?php echo URL?>vendors/css/admin/updateAccount1.css">
+
+
+     <div class="middle"> 
+     <a >UPDATE ACCOUNTS</a> </div>
+  
+
+  <div class="k1">  
+
+<div class="wrapper">
+    <div class="title">  Update Form  </div>
+
+    <div class="form">
+      
+        <div class="inputfield">
+          <label> Name</label>
+          <input type="text" class="input">
+       </div> 
+
+        <div class="inputfield">
+          <label>ID</label>
+          <input type="text" class="input">
+       </div> 
+
+       <div class="inputfield">
+          <label>Address</label>
+          <textarea class="textarea"></textarea>
+       </div> 
+
+        <div class="inputfield">
+          <label>Type(Landowner/Employee)</label>
+          <input type="text" class="input">
+       </div>
+
+        <div class="inputfield">
+          <label>Phone number</label>
+          <input type="text" class="input">
+       </div>
+
+       <div class="inputfield">
+          <label>Password</label>
+          <input type="password" class="input">
+       </div>  
+
+       <div class="inputfield">
+          <a>If you want to update the account</a>
+      
+       </div> 
+
+      
+      <div class="last">
+            <div class="a">
+       <a href="<?php echo URL?>admin/updateAccount"><input type="submit" value="Discard" class="btn"></a>
+      </div>
+
+
+       <div class="b">
+        <input type="submit" value="Update" class="btn">
+      </div>
+           
+
+      </div>
+
+     
+
+    </div>
+
+  </div> 
+</div>
+<?php include 'bottom-container.php';?>
---
+++ views/manager/bottom-container.php
@@ -0,0 +1,6 @@
+</section>
+  
+  <script src="<?php echo URL?>vendors/js/app.js"></script>
+
+</body>
+</html>
