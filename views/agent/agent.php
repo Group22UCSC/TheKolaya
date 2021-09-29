622ff6b28940730d9c31dd9e833375201fd21a4e
Roneki <roneki.saranga12@gmail.com>
2021-09-29 15:10:05 +0530

    change

--- views/agent/agent.php
+++ views/agent/agent.php
@@ -1,31 +1,55 @@
 <?php include 'topContainer.php';?>
 <link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/agent-dashboard.css">    
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<div class="availble list">
-<table class="table">
-    <thead>
-        <tr>
-            <th>Landowner ID</th>
-            <th>No of Containers (Estimated)<th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>L001</td>
-            <td>6<td>
-        </tr>
-        <tr>
-            <td>L002</td>
-            <td>4<td>
-        </tr>
-        <tr>
-            <td>L003</td>
-            <td>10<td>
-        </tr>
-    </tbody>    
-</table>
-
-   
-
+<div class="row">
+  <div class="column">
+    <table>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Points</th>
+      </tr>
+      <tr>
+        <td>Jill</td>
+        <td>Smith</td>
+        <td>50</td>
+      </tr>
+      <tr>
+        <td>Eve</td>
+        <td>Jackson</td>
+        <td>94</td>
+      </tr>
+      <tr>
+        <td>Adam</td>
+        <td>Johnson</td>
+        <td>67</td>
+      </tr>
+    </table>
+  </div>
+  <div class="column">
+    <table>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Points</th>
+      </tr>
+      <tr>
+        <td>Jill</td>
+        <td>Smith</td>
+        <td>50</td>
+      </tr>
+      <tr>
+        <td>Eve</td>
+        <td>Jackson</td>
+        <td>94</td>
+      </tr>
+      <tr>
+        <td>Adam</td>
+        <td>Johnson</td>
+        <td>67</td>
+      </tr>
+    </table>
+  </div>
+</div>
 <script src="<?php echo URL?>vendors/js/agent/dashboard.js"></script>
 <?php include 'bottomContainer.php';?>
