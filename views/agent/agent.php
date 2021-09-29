32df70da72e86f785d9e0abef2506a7dd01f9640
Melaka36 <melaka36pathiranagama@gmail.com>
2021-09-29 19:26:21 +0530

    chnges

--- views/agent/agent.php
+++ views/agent/agent.php
@@ -1,31 +1,60 @@
 <?php include 'topContainer.php';?>
-<link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/agent-dashboard.css">    
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<div class="availble list">
-<table class="table">
-    <thead>
-        <tr>
-            <th>Landowner ID</th>
-            <th>No of Containers (Estimated)<th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>L001</td>
-            <td>6<td>
-        </tr>
-        <tr>
-            <td>L002</td>
-            <td>4<td>
-        </tr>
-        <tr>
-            <td>L003</td>
-            <td>10<td>
-        </tr>
-    </tbody>    
-</table>
-
-   
+<link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/dashboard.css"> 
+<div class="text">
+    <h2>THINGS TO DO</h2>
+    </div>
+<div class="tables">
+  <div class="availablelist">
+    <h3>Available Landowner List </h3>
+    <table>
+      <tr>
+        <th>Landowner ID</th>
+        <th>Container Estimation</th>
+        
+      </tr>
+      <tr>
+        <td>L001</td>
+        <td>10</td>
+        
+      </tr>
+      <tr>
+        <td>L002</td>
+        <td>20</td>
+       
+      </tr>
+      <tr>
+        <td>L003</td>
+        <td>4</td>
+        
+      </tr>
+    </table>
+  </div>
+  <div class="deliverylist">
+    <h3>Delivery List </h3>
+    <table>
+    <tr>
+        <th>Landowner ID</th>
+        <th>Container Estimation</th>
+        
+      </tr>
+      <tr>
+        <td>L001</td>
+        <td>10</td>
+        
+      </tr>
+      <tr>
+        <td>L002</td>
+        <td>20</td>
+       
+      </tr>
+      <tr>
+        <td>L003</td>
+        <td>4</td>
+        
+      </tr>
+    </table>
+  </div>
+</div>
 
 <script src="<?php echo URL?>vendors/js/agent/dashboard.js"></script>
 <?php include 'bottomContainer.php';?>
