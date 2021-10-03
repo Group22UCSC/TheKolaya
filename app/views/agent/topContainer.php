4f9144e0d7dec56834075b772a947f0b431034d8
Roneki <roneki.saranga12@gmail.com>
2021-10-03 21:03:07 +0530

    modify

--- app/views/agent/topContainer.php
+++ app/views/agent/topContainer.php
@@ -1,28 +1,10 @@
-<!DOCTYPE html>
-<!-- Created by CodingLab |www.youtube.com/c/CodingLabYT-->
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
-    <title><?php echo TITLE?></title>
-    <link rel = "icon" href = "<?php echo URL?>vendors/images/thekolaya2.png" type = "image/x-icon">
-    <link rel="stylesheet" href="<?php echo URL?>vendors/css/style.css">
-    <link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/agent.css">
-    <!-- Boxicons CDN Link -->
-    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-<body>
   <div class="sidebar">
     <div class="logo-details">
         <div class="logo_name"><img src="<?php echo URL?>vendors/images/thekolaya-white.png" alt=""></div>
         <i class="fas fa-bars" id="btn"></i>
     </div>
     <ul class="nav-list">
-      <!-- <li>
-          <i class='bx bx-search' ></i>
-         <input type="text" placeholder="Search...">
-         <span class="tooltip">Search</span>
-      </li> -->
+     
       <li>
         <a href="<?php echo URL?>Agent">
           <i class="fas fa-th-large"></i>
@@ -58,52 +40,112 @@
        </a>
        <span class="tooltip">Confirm Deliverables</span>
      </li>
-     <li class="mobile-nav-icon">
-      <a href="#">
-        <i class="fas fa-bell"></i>
-        <span class="links_name">Notification</span>
-      </a>
-      <span class="tooltip">Notification</span>
-    </li>
-    <li class="mobile-nav-icon">
-      <a href="#">
-        <i class="fas fa-user"></i>
-        <span class="links_name">Profile</span>
-      </a>
-      <span class="tooltip">Profile</span>
-    </li>
-     <li>
-       <a href="#">
-        <i class="fas fa-sign-out-alt"></i>
-         <span class="links_name">Logout</span>
-       </a>
-       <span class="tooltip">Logout</span>
-     </li>
-     <!-- <li class="profile">
-         <div class="profile-details">
-           <img src="profile.jpg" alt="profileImg">
-           <div class="name_job">
-             <div class="name">Prem Shahi</div>
-             <div class="job">Web designer</div>
-           </div>
-         </div>
-         <i class='bx bx-log-out' id="log_out" ></i>
-     </li> -->
-    </ul>
+</ul>
+     <div class="social_media_icon">
+      <div class="social_media">
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+    
   </div>
   <section class="home-section">
-      <div class="header">
-        <div class="thekolya-logo"><img src="<?php echo URL?>vendors/images/thekolaya.png" alt=""></div>
-        <ul>
-          <li><i class="fas fa-bell"></i></li>
-          <li class="profile">
-            <div class="profile-details">
-              <div class="name_job">
-                <div class="name">Roneki<br><span>Agent</span></div>
+  <div class="wrapper">
+        <div class="navbar">
+          <div class="navbar_left">
+            <div class="thekolya-logo"><img src="<?php echo URL?>vendors/images/thekolaya.png" alt=""></div>
+          </div>
+      
+          <div class="navbar_right">
+            <div class="notifications">
+              <div id="bell" class="icon_wrap"><i class="far fa-bell"></i></div>
+              
+              <div class="notification_dd">
+                  <ul class="notification_ul">
+                      <?php
+                        for($i = 0; $i < 4; $i++) {
+                          echo '<li class="starbucks success">
+                                  <div class="notify_icon">
+                                    <span class="icon"><i class="fas fa-bell"></i></span>  
+                                  </div>
+                                  <div class="notify_data">
+                                      <div class="title">
+                                          Lorem, ipsum dolor.  
+                                      </div>
+                                      <div class="sub_title">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </div>
+                                  </div>
+                                  <div class="notify_status">
+                                      <p>Success</p>  
+                                  </div>
+                                </li>';
+                        }
+                      ?> 
+                      <li class="show_all">
+                          <p class="link">Show All Activities</p>
+                      </li> 
+                  </ul>
               </div>
-              <img src="<?php echo URL?>vendors/images/agent/profile.jpg" alt="profileImg">
+              
             </div>
-          </li>
-        </ul>
-      </div>      
-  
+            <div class="profile">
+              <div class="icon_wrap" id="account-web">
+                <img src="<?php echo URL?>vendors/images/agent/profile.jpg" alt="profile_pic">
+                <span class="name">Roneki</span>
+                <i class="fas fa-chevron-down"></i>
+              </div>
+      
+              <div class="profile_dd">
+                <ul class="profile_ul">
+                  <li class="profile_li">
+                      <div class="icon_wrap" id="account">
+                        <img src="<?php echo URL?>vendors/images/agent/profile.jpg" alt="profile_pic">
+                        <span class="name">Roneki</span>
+                      </div>
+                  </li>
+                  <li><a class="profile" href="<?php echo URL?>Agent/profile"><span class="picon"><i class="fas fa-user-alt"></i></span>Profile</a></li>
+                  <li><a class="settings" href="<?php echo URL?>Agent/editProfile"><span class="picon"><i class="fas fa-cog"></i></span>Settings</a></li>
+                  <li><a class="logout" href="<?php echo URL?>login/logout"><span class="picon"><i class="fas fa-sign-out-alt"></i></span>Logout</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="popup">
+          <div class="shadow"></div>
+          <div class="inner_popup">
+              <div class="notification_dd">
+                  <ul class="notification_ul">
+                      <li class="title">
+                          <p>All Notifications</p>
+                          <p class="close"><i class="fas fa-times" aria-hidden="true"></i></p>
+                      </li>
+                      <?php
+                        for($i = 0; $i < 6; $i++) {
+                          echo '<li class="starbucks success">
+                                  <div class="notify_icon">
+                                    <span class="icon"><i class="fas fa-bell"></i></span>  
+                                  </div>
+                                  <div class="notify_data">
+                                      <div class="title">
+                                          Lorem, ipsum dolor.  
+                                      </div>
+                                      <div class="sub_title">
+                                        Lorem ipsum dolor sit amet consectetur.
+                                    </div>
+                                  </div>
+                                  <div class="notify_status">
+                                      <p>Success</p>  
+                                  </div>
+                                </li>';
+                        }
+                      ?>
+                  </ul>
+              </div>
+          </div>
+        </div>
+        
+      </div>
