ea9e08fc61160f6322101d20657411f85c83dcdc
SasinduDias <sasiwijegunasinghe98@gmail.com>
2021-10-08 00:17:30 +0530

    create agent accounts ui fixed

--- app/views/admin/createAccountSelect.php
+++ app/views/admin/createAccountSelect.php
@@ -22,18 +22,18 @@
           <h3>Create Full Accounts</h3>
           <a href="<?php echo URL?>admin/DLandowner_createAccount">Direct Landowner</a>
           <a href="<?php echo URL?>admin/InDLandowner_createAccount">Indirect Landowner</a>
-          <a href="<?php echo URL?>admin/">Agent</a>
-          <a href="<?php echo URL?>admin/">Accountant</a>
-          <a href="<?php echo URL?>admin/">Product Manager</a>
-          <a href="<?php echo URL?>admin/">Supervisor</a>
-          <a href="<?php echo URL?>admin/">Manager</a>
-          <a href="<?php echo URL?>admin/">Admin</a>
+          <a href="<?php echo URL?>admin/create_agent">Agent</a>
+          <a href="<?php echo URL?>admin/create_accountant">Accountant</a>
+          <a href="<?php echo URL?>admin/create_Pmanager">Product Manager</a>
+          <a href="<?php echo URL?>admin/create_supervisor">Supervisor</a>
+          <a href="<?php echo URL?>admin/create_manager">Manager</a>
+          <a href="<?php echo URL?>admin/create_admin">Admin</a>
         </div>
         <div class="column">
           <h3>Create Temporary Accounts</h3>
           <a href="<?php echo URL?>admin/DLandowner_create_Temp_Account">Direct Landowner</a>
           <a href="<?php echo URL?>admin/InDLandowner_create_Temp_Account">Indirect Landowner</a>
-          <a href="<?php echo URL?>admin/">Agent</a>
+          <a href="<?php echo URL?>admin/create_agentTemp">Agent</a>
           <a href="<?php echo URL?>admin/">Accountant</a>
           <a href="<?php echo URL?>admin/">Product Manager</a>
           <a href="<?php echo URL?>admin/">Supervisor</a>
