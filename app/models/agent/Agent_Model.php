0459de7c08701c8d8162cc49aa4a49eda23bc54f
Roneki <roneki.saranga12@gmail.com>
2021-10-11 14:07:20 +0530

    connect available list table to database

--- app/models/agent/Agent_Model.php
+++ app/models/agent/Agent_Model.php
@@ -2,4 +2,55 @@
 
 class Agent_Model extends Model{
 
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function editProfile() {
+        $contact_number = $_SESSION['contact_number'];
+        $name = $_SESSION['name'];
+        $user_id = $_SESSION['user_id'];
+        $query = "UPDATE user SET contact_number='$contact_number', name='$name' WHERE user_id='$user_id'";
+        $this->db->runQuery($query);
+    }
+
+    function checkPassword($data) {
+        $user_id = $_SESSION['user_id'];
+        $query = "SELECT * FROM user WHERE user_id='$user_id'";
+        
+        $row = $this->db->runQuery($query);
+        $hashed_password = $row[0]['password'];
+
+        if(password_verify($data['password'], $hashed_password)) {
+            return $row;
+        }else {
+            return false;
+        }
+    }
+
+    function changePassword($data = []) {
+        $new_password = $data['new_password'];
+        $contact_number = $_SESSION['contact_number'];
+        
+        $query = "UPDATE user SET password='$new_password' WHERE contact_number='$contact_number'";
+        $row = $this->db->runQuery($query);
+        if($row) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    function availableListTable(){
+        $query = "SELECT user_id, no_of_estimated_containers FROM landowner WHERE route_no=1 AND landowner_type='indirect_landowner' AND tea_availability=1 ";
+        $row = $this->db->runQuery($query);
+        
+        if($row) {
+            return $row;
+        }else {
+            return false;
+        }
+    }
+    
 }
