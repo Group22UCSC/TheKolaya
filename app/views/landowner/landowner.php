1e43028fc8392ff87aed01968afc7a5b56bec2a8
Pasindu-Lakmal <pasindu1lak@gmail.com>
2021-10-15 08:39:35 +0530

    finalize dashboard

--- app/views/landowner/landowner.php
+++ app/views/landowner/landowner.php
@@ -1,11 +1,14 @@
 <?php include 'top-container.php'; ?>
 <!-- Top dashboard home content -->
 <link rel="stylesheet" href="<?php echo URL ?>vendors/css/landowner/landowner.css">
-
 <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+
+<!-- middle grid -->
+
 <div class="container">
 
-
+  <!-- first four boxes -->
   <div class="box-one">
     <div class="box">
       <div class="right-side">
@@ -19,6 +22,7 @@
       <i class='bx bxs-cart-add cart one'></i>
     </div>
   </div>
+
   <div class="box-two">
     <div class="box">
       <div class="right-side">
@@ -32,6 +36,7 @@
       <i class='bx bxs-cart-add cart two'></i>
     </div>
   </div>
+
   <div class="box-tree">
     <div class="box">
       <div class="right-side">
@@ -45,10 +50,11 @@
       <i class='bx bx-cart cart three'></i>
     </div>
   </div>
+
   <div class="box-four">
     <div class="box">
       <div class="right-side">
-        <div class="box-topic">Your Tea Rating</div>
+        <div class="box-topic">This Month Tea Rating</div>
         <div class="number">4.9</div>
         <div class="indicator">
           <i class='bx bx-down-arrow-alt down'></i>
@@ -59,18 +65,40 @@
       <i class='bx bxs-cart-download cart four'></i>
     </div>
   </div>
+
+  <!-- chart hedding -->
+
   <div class="chart-head">
     <h1>TEA SUPPLY DURING LAST WEEK</h1>
   </div>
+
+  <!-- chart -->
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
   <div class="chart">
     <div class="chartplace">
-      <canvas id="myChart1" style="width:100%;max-width:600px"></canvas>
+      <canvas id="myChart1" style="width:100%;max-width:1000px"></canvas>
     </div>
     <script>
       var xValues = ["Monday", "Tuesday,", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
       var yValues = [50, 25, 35, 44, 37, 80, 77];
-      var barColors = ["rgb(80, 200, 120)", "rgb(53, 83, 10)", "rgb(161, 242, 31)", "rgb(255, 213, 128)", "rgb(0, 57, 128)", "rgb(5, 17, 87)", "rgb(255, 140, 0)"];
+      var barColors = ['rgba(255, 99, 132, 0.8)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ];
+      var borderColor = [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ];
       var graphName;
       for (var i = 1; i <= 3; i++) {
         switch (i) {
@@ -86,7 +114,8 @@
             labels: xValues,
             datasets: [{
               backgroundColor: barColors,
-              data: yValues
+              data: yValues,
+              borderWidth: 2,
             }]
           },
           options: {
