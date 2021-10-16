c6edd78ff8c48031695cee93208766f48a204450
Roneki <roneki.saranga12@gmail.com>
2021-10-17 01:35:59 +0530

    complete update tea weight function in agent

--- app/controllers/agent/Agent.php
+++ app/controllers/agent/Agent.php
@@ -10,17 +10,46 @@ class Agent extends Controller{
 
       
        $result = $this->model->availablelistTable();
-      // print_r($result);
+    //    print_r($result);
        $this->view->render('Agent/Agent',$result);
     }
 
     // function availableLandownerList(){
     //     $this->view->showPage('Agent/AvailableList');
     // }
+        // public $weight_data = [
+        //      'date' => '',
+        //     'lid' => '',
+        //     'initial_weight' => '',
+        //     'emp_id_agent' => ''
+        // ];
 
-    // function updateTeaWeight(){
-    //     $this->view->showPage('Agent/popup');
-    // }
+
+    function updateTeaWeight(){  
+        $weight_data = [
+            'date' => '',
+           'lid' => '',
+           'initial_weight' => '',
+           'agent_id' => ''
+       ];      
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $this->weight_data['date'] = date("Y-m-d");
+            $this->weight_data['lid'] = trim($_POST['lid-popup']);
+            $this->weight_data['initial_weight'] = trim($_POST['weight-popup']);             
+            $this->weight_data['agent_id'] = $_SESSION['user_id'];
+            // print_r($_SESSION['user_id']);
+            // print_r($this->weight_data);
+            $this->model->updateWeight($this->weight_data);
+            $result = $this->model->availablelistTable();
+            //    print_r($result);
+               $this->view->render('Agent/Agent',$result);
+               //also add the set the tea availability of landowner to 0
+        }
+       
+    }
+
+
     function sendEmergencyMessage(){
         $this->view->showPage('Agent/EmergencyMessage');
     }
@@ -29,7 +58,7 @@ class Agent extends Controller{
         $this->view->showPage('Agent/previousUpdates');
     }
 
-    function confirmDeliverables(){
+    function confirmDeliverables(){       
         $this->view->showPage('Agent/DeliveryList');
     }
 
