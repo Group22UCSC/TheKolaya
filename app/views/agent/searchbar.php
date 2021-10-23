a0f2753e615870977c9f1db0d63e95d2a98d35b9
Roneki <roneki.saranga12@gmail.com>
2021-10-23 13:50:16 +0530

    modify

--- app/views/agent/searchbar.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/searchbar.css">
-
-<form class="searchform">    
-    <input type="text" id="search" placeholder="Enter Landowner ID.." onkeyup="searchTable()">
-    <!-- <input type="submit" value="search" id="submit"> -->
-</form>
