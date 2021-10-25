f11781abac2096256f4a00ea9ae2a37cd549c820
Roneki <roneki.saranga12@gmail.com>
2021-10-25 20:29:26 +0530

    view previous tea updates

--- app/views/agent/previousUpdates.php
+++ app/views/agent/previousUpdates.php
@@ -6,7 +6,8 @@
     <link rel = "icon" href = "<?php echo URL?>vendors/images/thekolaya2.png" type = "image/x-icon">
     <link rel="stylesheet" href="<?php echo URL?>vendors/css/style.css">
     <link rel="stylesheet" href="<?php echo URL?>vendors/css/nav-style.css">       
-    <link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/preupdates.css">    
+    <link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/preupdates.css">  
+    <script src="<?php echo URL?>vendors/js/agent/preupdates.js"></script>    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>     
     <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,10 +17,11 @@
 <div class="topic">View Previous Updates</div>
 
 <div class="update_buttons">
-<button class="tea"> Tea Updates </button>
-<button class="request"> Request Updates </button>
+<button class="tea" onclick="loadTeaUpdates()"> Tea Updates </button>
+<button class="request" onclick="loadRequestUpdates()"> Request Updates </button>
 
 </div>
 
 
 <?php include 'bottomContainer.php';?>
+
