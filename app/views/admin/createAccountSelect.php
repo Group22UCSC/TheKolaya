57697175df38c89ac9fae2d231a3473e0dd7b094
sasinduDias <sasiwijegunasinghe98@gmail.com>
2021-10-26 22:05:34 +0530

    admin create accounts changed

--- app/views/admin/createAccountSelect.php
+++ app/views/admin/createAccountSelect.php
@@ -4,51 +4,13 @@
  <link rel="stylesheet" href="<?php echo URL?>vendors/css/admin/createAccountSelect.css">
 
  <div class="first">CREATE ACCOUNTS</div>
+<div class="middle">
+        <div class="btn-list">
+          <a href="<?php echo URL?>manager/viewFertilizer"> <button class="btn btn-account"><i class="fas fa-luggage-cart"></i> CREATE FULL ACCOUNT</button></a>
 
-    <div class="middle">
-       
-            <div class="navbar">
- 
-  <div class="dropdown">
-    <button class="dropbtn">Hey! Click here to create account 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-     <!--  <div class="header">
-        <h2>Mega Menu</h2>
-      </div>  -->  
-      <div class="row">
-        <div class="column">
-          <h3>Create Full Accounts</h3>
-          <a href="<?php echo URL?>admin/agent_land_account">Direct Landowner</a>
-          <a href="<?php echo URL?>admin/agent_land_account">Indirect Landowner</a>
-          <a href="<?php echo URL?>admin/agent_land_account">Agent</a>
-          <a href="<?php echo URL?>admin/create_account">Accountant</a>
-          <a href="<?php echo URL?>admin/create_account">Product Manager</a>
-          <a href="<?php echo URL?>admin/create_account">Supervisor</a>
-          <a href="<?php echo URL?>admin/create_account">Manager</a>
-      
+           <a href="<?php echo URL?>manager/viewFirewood"> <button class="btn btn-tea"><i class="fas fa-dumpster-fire"></i>CREATE TEMPORARY ACCOUNT</button></a>
+
         </div>
-        <div class="column">
-          <h3>Create Temporary Accounts</h3>
-          <a href="<?php echo URL?>admin/agent_land_tempAccount">Direct Landowner</a>
-          <a href="<?php echo URL?>admin/agent_land_tempAccount">Indirect Landowner</a>
-          <a href="<?php echo URL?>admin/agent_land_tempAccount">Agent</a>
-          <a href="<?php echo URL?>admin/create_tempAccount">Accountant</a>
-          <a href="<?php echo URL?>admin/create_tempAccount">Product Manager</a>
-          <a href="<?php echo URL?>admin/create_tempAccount">Supervisor</a>
-          <a href="<?php echo URL?>admin/create_tempAccount">Manager</a>
-         
-        </div>
-      </div>
-    </div>
-  </div> 
-</div>
-
-
-
-
-
     </div>
     
 <?php include 'bottom-container.php';?>
