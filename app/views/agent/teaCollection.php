1f75e83f3ea7199f35eeb234db4656fc2e212103
Kumudperera <nethmalperera54@gmail.com>
2021-11-04 19:26:46 +0530

    Agent modified

--- app/views/agent/teaCollection.php
+++ app/views/agent/teaCollection.php
@@ -1,27 +1,30 @@
+<link rel="stylesheet" href="<?php echo URL ?>vendors/css/agent/teacollection.css">
 
-    <link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/teacollection.css">  
+<div class="page" id="teapopup">
+    <div class="title">
+        Update Tea Weight
+    </div>
 
-      <div class="page" id="teapopup">
-        <div class="title">
-           Update Tea Weight
-        </div>
-        <div class="form">
-           
+    <div class="form">
+        <form action="#" method="POST" id="teaUpdateForm">
             <div class="inputfield">
                 <label>Landowner ID</label>
                 <input type="text" class="input" id="lid" name="lid" readonly>
-            </div>  
+            </div>
             <div class="inputfield">
                 <label>Initial Tea Weight</label>
                 <input type="text" class="input" name="weight" id="weight" required>
-            </div>        
-            <div class="inputfield" id="btnset">
-            <button class="back" onclick="closeteaform()">Back</button>
-            <button class  = "btn" id="myBtn" onclick = "openpopup()"> Add<span> Weight</span> </button>
-            <!-- <input type="submit" value="Add Weight" class="btn" id="myBtn"  onclick="openpopup()" >             -->
             </div>
-       
-        </div>
-      </div>	     
+            <div class="inputfield" id="btnset">
+                <button class="back" onclick="closeteaform()">Back</button>
+                <button class="btn" id="myBtn"> Add<span> Weight</span> </button>
+                <!-- <input type="submit" value="Add Weight" class="btn" id="myBtn"  onclick="openpopup()" >             -->
+            </div>
+        </form>
 
-      <!-- onclick="openpopup()" -->
+
+    </div>
+</div>
+
+<script></script>
+<!-- onclick="openpopup()" -->
