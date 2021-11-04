1eb7d763b23c4c9ad764dc2f207910cea6a5fdbd
Kumudperera <nethmalperera54@gmail.com>
2021-11-04 13:48:19 +0530

    Supervisor modified

--- app/views/supervisor/supervisor.php
+++ app/views/supervisor/supervisor.php
@@ -32,15 +32,14 @@
       <div class="table-wrapper">
         <div class="table_header">Today tea collection</div>
         <div class="table" id="today_collection_table">
-          
+          <div class="row tabel-header">
+            <div class="cell">Landowner ID</div>
+            <div class="cell">Tea Weight(kg)</div>
+            <div class="cell">Agent ID</div>
+          </div>
           <?php
 
           if (!empty($data1)) {
-            echo '<div class="row tabel-header">
-                    <div class="cell">Landowner ID</div>
-                    <div class="cell">Tea Weight(kg)</div>
-                    <div class="cell">Agent ID</div>
-                  </div>';
             for ($i = 0; $i < count($data1); $i++) {
               echo '<div class="row">
                       <div class="cell" data-title="Landowener_id">' . $data1[$i]['lid'] . '</div>
@@ -48,26 +47,28 @@
                       <div class="cell" data-title="Agent_id">' . $data1[$i]['agent_id'] . '</div>
                     </div>';
             }
-          } else {
-            echo '<div id="not_display_collection_yet" style="border-radius: 0px; color:red; background-color: white;" class="table_header" >There is no tea collection to update</div>';
           }
           ?>
         </div>
       </div>
+      <?php
+      if (empty($data1)) {
+        echo '<div id="not_display_collection_yet" style="border-radius: 0px; color:red; background-color: white;" class="table_header" >There is no tea collection to update</div>';
+      }
+      ?>
     </div>
 
     <div class="request-table">
       <div class="table-wrapper">
         <div class="table_header">Today Fertilizer Requests</div>
         <div class="table" id="today_request_table">
-          
+          <div class="row tabel-header">
+            <div class="cell">Landowner ID</div>
+            <div class="cell">Name</div>
+            <div class="cell">Reqeust Amount</div>
+          </div>
           <?php
           if (!empty($data2)) {
-            echo '<div class="row tabel-header">
-                    <div class="cell">Landowner ID</div>
-                    <div class="cell">Name</div>
-                    <div class="cell">Reqeust Amount</div>
-                  </div>';
             for ($i = 0; $i < count($data2); $i++) {
               echo '<div class="row">
                     <div class="cell" data-title="Landowener_id">' . $data2[$i]['user_id'] . '</div>
@@ -75,30 +76,40 @@
                     <div class="cell" data-title="Amount">' . $data2[$i]['amount'] . '</div>
                   </div>';
             }
-          } else {
-            echo '<div id="not_display_request_yet" style="border-radius: 0px; color:red; background-color: white;" class="table_header" >There is no Request to update</div>';
           }
           ?>
         </div>
       </div>
+      <?php
+      if (empty($data2)) {
+        echo '<div id="not_display_request_yet" style="border-radius: 0px; color:red; background-color: white;" class="table_header" >There is no Request to update</div>';
+      }
+      ?>
       <div class="table-row">
         <a href="<?php echo URL ?>Supervisor/manageRequests"><button class="table-btn">Go to confirm</button></a>
       </div>
     </div>
 
   </div>
+  <script src="<?php echo URL ?>vendors/js/jquery-3.6.0.min.js"></script>
+  <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
   <script>
-    $(document).ready(function() {
-      $('.agent-tea-table').click(function(event) {
-        console.log(event);
-        $.ajax({
-          url: '<?php echo URL ?>Supervisor',
-          type: 'GET',
-          dataType: "html",
-          success: function(response) {
-            $("#table-container").html(data);
-          }
-        });
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('ef64da0120ca27fe19a3', {
+      cluster: 'ap1'
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      // alert(JSON.stringify(data));
+      $.ajax({
+        url: "<?php echo URL ?>Supervisor/getAgentTeaCollection",
+        success: function(result) {
+          $('#not_display_collection_yet').hide();
+          $('#today_collection_table').append(result);
+        }
       });
     });
   </script>
