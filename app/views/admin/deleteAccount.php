79d5737bbfacba7c687d6d9b29a0a31dc7240c55
sasinduDias <sasiwijegunasinghe98@gmail.com>
2021-11-24 10:26:50 +0530

    admin delete account modified

--- app/views/admin/deleteAccount.php
+++ app/views/admin/deleteAccount.php
@@ -22,7 +22,7 @@
                            <tr class="header">
                                 <th style="width:33.33%;">Name</th>
                                 <th style="width:33.33%;">ID</th>
-                                <th style="width:33.33%;">Type</th>
+                                <th style="width:33.33%;">Contact Number</th>
    
                             </tr>
                        <div class="vertical">
@@ -33,7 +33,7 @@
                                  echo '<tr id="tea" data-href="">     
                                            <td>'.$data[$i]['name'].'</td>
                                            <td>'.$data[$i]['user_id'].'</td>
-                                           <td>'.$data[$i]['user_type'].'</td>
+                                           <td>'.$data[$i]['contact_number'].'</td>
                                            
                                        </tr>';                
                                }       
@@ -45,52 +45,66 @@
   
                       </table>
 
+            //table data get auto filled form
+            <script>
+
+              var table=document.getElementById('myTable'),rIndex;
+              for (var i = 1; i < table.rows.length; i++) {
+                   table.rows[i].onclick=function()
+                   {
+                    //rIndex=this.rowIndex;
+                    document.getElementById("fname").value=this.cells[0].innerHTML;
+                     document.getElementById("id").value=this.cells[1].innerHTML;
+                      document.getElementById("Phone").value=this.cells[2].innerHTML;
+                   };
+                 }
 
 
-                                <!--  // script for filtering -->
-                                  <script>
-                                  function myFunction() {
-                                    var input, filter, table, tr, td, i, txtValue;
-                                    input = document.getElementById("myInput");
-                                    filter = input.value.toUpperCase();
-                                    table = document.getElementById("myTable");
-                                    tr = table.getElementsByTagName("tr");
-                                    for (i = 0; i < tr.length; i++) {
-                                      td = tr[i].getElementsByTagName("td")[0];
-                                      if (td) {
-                                        txtValue = td.textContent || td.innerText;
-                                        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                                          tr[i].style.display = "";
-                                        } else {
-                                          tr[i].style.display = "none";
-                                        }
-                                      }       
-                                    }
-                                  }
+            </script>          
+
+
+
+
+
+
+             <!--  // script for filtering -->
+               <script>
+               function myFunction() {
+                 var input, filter, table, tr, td, i, txtValue;
+                 input = document.getElementById("myInput");
+                 filter = input.value.toUpperCase();
+                 table = document.getElementById("myTable");
+                 tr = table.getElementsByTagName("tr");
+                 for (i = 0; i < tr.length; i++) {
+                   td = tr[i].getElementsByTagName("td")[0];
+                   if (td) {
+                     txtValue = td.textContent || td.innerText;
+                     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                       tr[i].style.display = "";
+                     } else {
+                       tr[i].style.display = "none";
+                     }
+                   }       
+                 }
+               }
+               
+               </script>
                                   
-                                  </script>
-                                  
 
 
 
-                                 <!-- script for view table -->
-                                 <script>
-                                   document.addEventListener("DOMContentLoaded",() => {
-                                 const rows = document.querySelectorAll("tr[data-href]");
-                                 rows.forEach(row =>{
-                                     row.addEventListener("click", ()=>{
-                                      // openteaform();
-                         window.location.href=row.dataset.href;
-
-                                     });
-                                 });
-                             });
-
-                                        
-                                 
-                                 </script>
-
-
+            <!-- script for view table -->
+           <!--  <script>
+              document.addEventListener("DOMContentLoaded",() => {
+                const rows = document.querySelectorAll("tr[data-href]");
+                  rows.forEach(row =>{
+                       row.addEventListener("click", ()=>{
+                        // openteaform();
+                        window.location.href=row.dataset.href;
+                       });
+                   });
+               });
+           </script> -->
  
      </div>
   </div>
@@ -106,22 +120,22 @@
 
 <div class="wrapper1">
     <div class="title">  Delete Form  </div>
-
+      <form action="<?php echo URL?>admin/deleteAccount" method="POST">
     <div class="form">
       
         <div class="inputfield">
           <label> Name</label>
-          <input type="text" class="input">
+          <input type="text" name="fname" id="fname" class="input">
        </div> 
 
         <div class="inputfield">
           <label>ID</label>
-          <input type="text" class="input">
+          <input type="text" name="id" id="id" class="input">
        </div> 
 
         <div class="inputfield">
           <label>Phone number</label>
-          <input type="text" class="input">
+          <input type="text" name="phone" id="Phone" class="input">
        </div>
 
       <!--  <div class="inputfield">
