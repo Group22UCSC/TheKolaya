18b7049e4cf44bd1a1bd3e5e8dbf428d2e2327b4
Kumudperera <nethmalperera54@gmail.com>
2021-11-26 21:15:51 +0530

    App.php

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -6,14 +6,14 @@ class Controller {
         $this->view = new View();
     }
 
-    public function loadModel($fileName,$modelName) {
-        $path = '../app/models/'.$fileName.'/'. $modelName. '_Model.php';      
-        if(file_exists($path)) {
-            require $path;
-            $className = $modelName . '_Model';
-            $this->model = new $className();
-        }
-    }
+    // public function loadModel($fileName,$modelName) {
+    //     $path = '../app/models/'.$fileName.'/'. $modelName. '_Model.php';      
+    //     if(file_exists($path)) {
+    //         require $path;
+    //         $className = $modelName . '_Model';
+    //         $this->model = new $className();
+    //     }
+    // }
 
     public function loadModelUser($modelName) {
         $path = '../app/models/'. $modelName. '_Model.php';
