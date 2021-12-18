8292b178519aee501193a2afd583757ef7db6c9f
Roneki <roneki.saranga12@gmail.com>
2021-12-19 01:38:07 +0530

    complete emergency message db connection

--- app/controllers/Agent.php
+++ app/controllers/Agent.php
@@ -61,11 +61,28 @@ class Agent extends Controller
         }
     }
 
-
+    //load emergency message page
     function viewEmergencyMessage()
     {
         $this->view->showPage('Agent/EmergencyMessage');
     }
+
+    //send emergency message to manager
+    function sendEmergencyMessage(){
+        $msg_data = [
+            'message' => '',           
+            'agent_id' => ''
+        ]; 
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $this->msg_data['message'] = trim($_POST['message']);
+            $this->msg_data['agent_id'] = $_SESSION['user_id'];
+            $this->model->storeEmergencyMessage($this->msg_data);
+            print_r($this->msg_data);
+            $this->view->showPage('Agent/EmergencyMessage');
+    }
+}
 
     function viewPreviousUpdates()
     {
--- app/views/agent/emergencyMessage.php
+++ app/views/agent/emergencyMessage.php
@@ -11,8 +11,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>                
     <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>  
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script> -->
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>
@@ -26,11 +26,11 @@
         <form class="emergencyForm" id="emergencyForm">
             <div class="inputfield">
                 <label>Route No</label>
-                <input type="text" id="route_no" value="<?php echo $_SESSION['route']?>" class="input" required>
+                <input type="text" id="route_no" name="route_no" value="<?php echo $_SESSION['route']?>" class="input" required>
             </div>  
             <div class="inputfield">
                 <label>Message</label>
-               <textarea class="input" rows=5 columns=50 id="message" placeholder="Type your message here."></textarea>
+               <textarea class="input" rows=5 columns=50 id="message" name="message" placeholder="Type your message here."></textarea>
             </div>        
             <div class="inputfield">
             <input type="submit" value="Send" class="btn" id="link">
@@ -40,55 +40,58 @@
       </div>	              
 <?php include 'bottomContainer.php';?>
 
-<script>
-  $(document).on('click', '#link', function(e) {
-		    swal({
-				title: "Are you sure ?", 
-				type: "warning",
-				confirmButtonText: "Yes",
-				showCancelButton: true,
-        confirmButtonColor:'#4DD101',
-        cancelButtonColor:'#FF2400',
-        confirmButtonText: 'Yes, Send it!'
-		    })
-		    	.then((result) => {
-					if (result.value) {
-            swal(
-				      '<b style="color:green;"> Message Sent',
-				      'You clicked the <b style="color:green;">Success</b> button!',
-			      	'success'
-			)
-					} else if (result.dismiss === 'cancel') {
-					    swal(
-					      'Cancelled',
-					      'Type another message',
-					      'error'
-					    )
-					}
-				})
-		});
-    
-    $(document).ready(function () {
-      $('#link').click(function(event) {       
+
+<script src="<?php echo URL ?>vendors/js/supervisor/sweetalert2.all.min.js"></script>
+  <script src="<?php echo URL ?>vendors/js/jquery-3.6.0.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#link').click(function(event) {
+        event.preventDefault();
         var form = $('#emergencyForm').serializeArray();
         // console.log(form);
         var route = $('#route_no').val();
-        var message = $('#message').val();
-    $.ajax({
-      type: "POST",
-      url: "<?php echo URL ?>agent/sendEmergencyMessage",
-      cache: false,
-      data: form,
-    }).done(function (response) {      
-      console.log("success");
-      // console.log(response);     
-      // $('#emergencyForm').html(response);
-      
-    });
-    console.log('route'+route);
-    console.log('message'+message);
-    event.preventDefault();
-  });
-});
-    </script>
-
+        var message = $('#message').val();   
+        Swal.fire({
+          title: 'Are you sure to send the message?',         
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#01830c',
+          cancelButtonColor: '#ff3300',
+          confirmButtonText: 'Yes, Send!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            $("#emergencyForm").trigger("reset");
+            $.ajax({
+              type: "POST",
+              url: "<?php echo URL ?>agent/sendEmergencyMessage",
+              cache: false,
+              data: form,
+              success: function(response) {
+                Swal.fire({
+                  icon: 'success',
+                  title: 'Sending Success !',
+                  text: 'Your message has been sent.',
+                  confirmButtonColor: '#01830c'
+                }).then((result) => {
+                  location.reload();                      
+                })
+                // console.log(data);
+                console.log("success");
+                console.log(response);
+                console.log('route'+route);
+                console.log('message'+message);
+              },
+              error: function(xhr, ajaxOptions, thrownError) {
+                Swal.fire({
+                  icon: 'error',
+                  title: 'Oops...',
+                  text: 'Something went wrong! ' + xhr.status + ' ' + thrownError,
+                  // footer: '<a href="">Why do I have this issue?</a>'
+                })
+              }
+            })
+          }
+        })
+      })
+    })
+  </script>
