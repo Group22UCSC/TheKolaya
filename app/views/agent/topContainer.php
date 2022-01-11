79d07f5d0db6640cbfd31f9a68643429837a9a07
Roneki <roneki.saranga12@gmail.com>
2022-01-11 18:30:24 +0530

    modify assign agent

--- app/views/agent/topContainer.php
+++ app/views/agent/topContainer.php
@@ -99,18 +99,18 @@
             <div class="noti-modal">
               <div class="noti-modal-content">
                 <span class="noti-modal-close-button">×</span>
-                <div class="title">
+                <!-- <div class="title">
                   Emergency Agent Assign
                </div>  
                <label class="msg">You have been assigned to the following route due to an emergency. </label>   
-                 <!-- <input type="text" id="route_no" name="route_no" value="2" class="input" readonly>  -->
+                 
                  <br>
                  <label id="routeno"> Route No  : 2</label>
                                       
                 <div class="inputfield" id="btnset">
                 <button class="assignconfirm" id="assignconfirm" onclick="confirmassign()"> Confirm</button>
                 <button class="assignreject" onclick="rejectassign()">Reject</button>               
-            </div>
+            </div> -->
               </div>
             </div>
           </div>
