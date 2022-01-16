05d90879af171108079ffc77585620475a98279e
Melaka36 <melaka36pathiranagama@gmail.com>
2022-01-16 17:31:02 +0530

    PM dashboard bar chart done

--- app/views/productmanager/productmanager.php
+++ app/views/productmanager/productmanager.php
@@ -5,6 +5,69 @@
   <link rel="stylesheet" href="<?php echo URL ?>vendors/css/<?php echo $_SESSION['user_type'] ?>/<?php echo $_SESSION['user_type'] ?>-style.css">
   <link rel="stylesheet" href="<?php echo URL ?>vendors/css/<?php echo $_SESSION['user_type'] ?>/<?php echo $_SESSION['user_type'] ?>-queries.css">
    
+  <?php 
+
+print_r($data);
+// print_r($data);
+$today = date("Y-m-d");
+$len = sizeof($data);
+
+
+// $month=date("m");
+// $date = $data[0]['date'];
+// echo date('F, Y');
+$month06start = date('m', strtotime("0 month"));  // this month
+$month05start = date('m', strtotime("-1 month"));  //  prev month
+$month04start = date('m', strtotime("-2 month")); // 2 onths ago
+$month03start = date('m', strtotime("-3 month")); // 3months ago
+$month02start = date('m', strtotime("-4 month")); // 4 months ago
+$month01start = date('m', strtotime("-5 month")); // 5 months ago
+
+// echo $month06start;
+$tea = array('0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'); // array to store tea net weights of each month
+// (int)$tea[11]=(int)$tea[11]+5;
+// echo (int)$tea[11];
+// echo $len;
+$tea6 = 0.0; // this month tea quantity
+$tea5 = 0.0; // previous month tea quantity
+$tea4 = 0.0; // 2 months ago tea quantity
+$tea3 = 0.0;
+$tea2 = 0.0;
+$tea1 = 0.0;
+
+// tea quality variables
+$starts5 = 0;
+$starts4 = 0;
+$starts3 = 0;
+$starts2 = 0;
+$starts1 = 0;
+for ($i = 0; $i < $len; $i++) {
+
+  $dateTest = $data[$i]['date']; //get the date of which the net weight was added
+  $dateTest2 = date('Y-m-d', strtotime($dateTest));
+  $month = date('m', strtotime($dateTest2));
+  // echo $month;
+  // (int)$tea[$month] += (int)$data[$i]['net_weight'];
+  if ($month == $month06start) {  // if date is this month's date
+    $tea6 += (float)$data[$i]['sold_price'] * (float)$data[$i]['sold_amount'];
+  } else if ($month == $month05start) { // if date is previous month's date
+    $tea5 += (float)$data[$i]['sold_price'] * (float)$data[$i]['sold_amount'];
+  } else if ($month == $month04start) {
+    $tea4 += (float)$data[$i]['sold_price'] * (float)$data[$i]['sold_amount'];
+  } else if ($month == $month03start) {
+    $tea3 += (float)$data[$i]['sold_price'] * (float)$data[$i]['sold_amount'];
+  } else if ($month == $month02start) {
+    $tea2 += (float)$data[$i]['sold_price'] * (float)$data[$i]['sold_amount'];
+  } else if ($month == $month01start) {
+    $tea1 += (float)$data[$i]['sold_price'] * (float)$data[$i]['sold_amount'];
+  } else {
+  }
+
+}
+
+?>
+
+
   <!-- toop button container -->
   <div class="home-content">
     <div class="overview-boxes">
@@ -63,8 +126,21 @@
       <canvas id="myChart1" style="width:100%;max-width:600px"></canvas>
 
       <script>
-        var xValues = ["January", "February", "March", "April", "May", "June"];
-        var yValues = [4500, 4900, 3400, 2400, 4500, 4200];
+        val06 = '<?php echo $tea6; ?>'; // get the valu from php to js
+        val05 = '<?php echo $tea5; ?>';
+        val04 = '<?php echo $tea4; ?>';
+        val03 = '<?php echo $tea3; ?>';
+        val02 = '<?php echo $tea2; ?>';
+        val01 = '<?php echo $tea1; ?>';
+        var m = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var last_n_months = []
+        var d = new Date()
+        for (var i = 5; i >= 0; i--) {
+          last_n_months[i] = m[d.getMonth()]
+          d.setMonth(d.getMonth() - 1)
+        }
+        var xValues = last_n_months;
+        var yValues = [val01, val02, val03, val04, val05, val06];
         var barColors = ["#2BD47D", "#ffc233", "#e05260", "#66b0ff", "#3a5ec0"];
 
         new Chart("myChart1", {
