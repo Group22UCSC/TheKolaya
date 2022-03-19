14eff0b6ec62035a3b3b51992f7998b86017d060
Roneki <roneki.saranga12@gmail.com>
2022-03-20 00:34:54 +0530

    make responsive preteaupdates.css

--- app/views/agent/noSearchUpdates.php
+++ app/views/agent/noSearchUpdates.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" href="<?php echo URL ?>vendors/css/agent/noSearchUpdates.css">
-    
+<div class = "sad">
+<img src="<?php echo URL?>vendors/images/agent/sad.jpg" alt="sad">
+</div>
         <div class="note">Sorry, No previous updates found!</div>        
    
