68c3706b72d3f17dc727e5d373d69d5963f1aa0d
Roneki <roneki.saranga12@gmail.com>
2022-03-21 14:29:02 +0530

    make responsive teacollection.css

--- app/views/agent/topContainer.php
+++ app/views/agent/topContainer.php
@@ -1,69 +1,69 @@
 <link rel="stylesheet" href="<?php echo URL ?>vendors/css/style.css">
 <link rel="stylesheet" href="<?php echo URL ?>vendors/css/nav-style.css">
 <link rel="stylesheet" href="<?php echo URL ?>vendors/css/nav-style2.css">
-<link rel="stylesheet" href="<?php echo URL?>vendors/css/agent/agent.css">
+<link rel="stylesheet" href="<?php echo URL ?>vendors/css/agent/agent.css">
 <link rel="stylesheet" href="<?php echo URL ?>vendors/css/agent/notification.css">
-  <div class="sidebar">
-    <div class="logo-details">
-        <div class="logo_name"><img src="<?php echo URL?>vendors/images/thekolaya-white.png" alt=""></div>
-        <i class="fas fa-bars" id="btn"></i>
-    </div>
-    <ul class="nav-list">
-     
-      <li>
-        <a href="<?php echo URL?>Agent">
-          <i class="fas fa-th-large"></i>
-          <span class="links_name">Dashboard</span>
-        </a>
-         <span class="tooltip">Dashboard</span>
-      </li>
-      <li>
-       <a href="<?php echo URL?>Agent/availableLandownerList">
-        <i class="fa fa-list" aria-hidden="true"></i>
-         <span class="links_name">Available Landowners</span>
-       </a>
-       <span class="tooltip">Available Landowners</span>
-     </li>
-     <li>
-       <a href="<?php echo URL?>Agent/confirmDeliverables">
-        <i class="fa fa-check-square" aria-hidden="true"></i>
-         <span class="links_name">Confirm Deliverables</span>
-       </a>
-       <span class="tooltip">Confirm Deliverables</span>
-     </li>
-     <li>
-       <a href="<?php echo URL?>Agent/viewPreviousUpdates">
-        <i class="fa fa-balance-scale" aria-hidden="true"></i>
-         <span class="links_name">View Previous Updates</span>
-       </a>
-       <span class="tooltip">View Previous Updates</span>
-     </li>
-     <li>
-       <a href="<?php echo URL?>Agent/viewEmergencyMessage">
-        <i class="fa fa-envelope" aria-hidden="true"></i>
-         <span class="links_name">Emergency Message</span>
-       </a>
-       <span class="tooltip">Emergency Message</span>
-     </li>
-     
-</ul>
-     <div class="social_media_icon">
-      <div class="social_media">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-      </div>
-    </div>
-    
+<div class="sidebar">
+  <div class="logo-details">
+    <div class="logo_name"><img src="<?php echo URL ?>vendors/images/thekolaya-white.png" alt=""></div>
+    <i class="fas fa-bars" id="btn"></i>
   </div>
-  <section class="home-section">
+  <ul class="nav-list">
+
+    <li>
+      <a href="<?php echo URL ?>Agent">
+        <i class="fas fa-th-large"></i>
+        <span class="links_name">Dashboard</span>
+      </a>
+      <span class="tooltip">Dashboard</span>
+    </li>
+    <li>
+      <a href="<?php echo URL ?>Agent/availableLandownerList">
+        <i class="fa fa-list" aria-hidden="true"></i>
+        <span class="links_name">Available Landowners</span>
+      </a>
+      <span class="tooltip">Available Landowners</span>
+    </li>
+    <li>
+      <a href="<?php echo URL ?>Agent/confirmDeliverables">
+        <i class="fa fa-check-square" aria-hidden="true"></i>
+        <span class="links_name">Confirm Deliverables</span>
+      </a>
+      <span class="tooltip">Confirm Deliverables</span>
+    </li>
+    <li>
+      <a href="<?php echo URL ?>Agent/viewPreviousUpdates">
+        <i class="fa fa-balance-scale" aria-hidden="true"></i>
+        <span class="links_name">View Previous Updates</span>
+      </a>
+      <span class="tooltip">View Previous Updates</span>
+    </li>
+    <li>
+      <a href="<?php echo URL ?>Agent/viewEmergencyMessage">
+        <i class="fa fa-envelope" aria-hidden="true"></i>
+        <span class="links_name">Emergency Message</span>
+      </a>
+      <span class="tooltip">Emergency Message</span>
+    </li>
+
+  </ul>
+  <div class="social_media_icon">
+    <div class="social_media">
+      <a href="#"><i class="fab fa-facebook-f"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+    </div>
+  </div>
+
+</div>
+<section class="home-section">
   <div class="wrapper">
-        <div class="navbar">
-          <div class="navbar_left">
-            <div class="thekolya-logo"><img src="<?php echo URL?>vendors/images/thekolaya.png" alt=""></div>
-          </div>
-      
-          <div class="navbar_right">
+    <div class="navbar">
+      <div class="navbar_left">
+        <div class="thekolya-logo"><img src="<?php echo URL ?>vendors/images/thekolaya.png" alt=""></div>
+      </div>
+
+      <div class="navbar_right">
         <!-- Newly added -->
         <div class="icons">
           <div class="notification">
