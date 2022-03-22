6424f2f222ef55a4bd9e9c13e8e98cb0c76c6b38
sasinduDias <sasiwijegunasinghe98@gmail.com>
2022-03-22 09:45:05 +0530

    admin errors fixed

--- vendors/js/admin/updatejs.php
+++ vendors/js/admin/updatejs.php
@@ -6,53 +6,51 @@
             var form = $('#updateForm').serializeArray();
              // var user_id=document.getElementById("id").val;
 
-    var str="Update details set on ?";
+            // var str="Update details set on ?";
 
-    Swal.fire({
-      title: 'Are You Sure ?',
-      icon: 'warning',
-    //   html:'<div>Line0<br />Line1<br /></div>',
-    html: '<pre>' + str + '</pre>',
-      //text: "Price Per Unit:  "+amount+"Amount: "+"<br>"+"Amount",
-      confirmButtonColor: '#FF2400',
-      cancelButtonColor: '#4DD101',
-      confirmButtonText: 'Update!',
-      showCancelButton: true
-      }).then((result) => {
-          if (result.isConfirmed) {
-              $("#updateForm").trigger("reset");
+    // Swal.fire({
+    //   title: 'Are You Sure ?',
+    //   icon: 'warning',
+    // //   html:'<div>Line0<br />Line1<br /></div>',
+    // html: '<pre>' + str + '</pre>',
+    //   //text: "Price Per Unit:  "+amount+"Amount: "+"<br>"+"Amount",
+    //   confirmButtonColor: '#FF2400',
+    //   cancelButtonColor: '#4DD101',
+    //   confirmButtonText: 'Update!',
+    //   showCancelButton: true
+    //   }).then((result) => {
+    //       if (result.isConfirmed) {
+    //           $("#updateForm").trigger("reset");
               
-              $.ajax({
-                  type: "POST",
-                  url: "<?php echo URL?>Admin/updateAccount",
+    //           $.ajax({
+    //               type: "POST",
+    //               url: "<?php echo URL?>Admin/updateAccount",
                   
-                  data: form,
-                  success: function(data) {
-                      console.log(data);
-                      Swal.fire(
-                      'Updated!',
-                      'Your Record Was Updated Succesfully.',
-                      'success'
-                      ).then(() => {
-                        location.reload();
-                      })
-                      // clearTable();
-                      // getTable();
-                      // checkForm();
-                  },
-                  error : function (xhr, ajaxOptions, thrownError) {
-                      Swal.fire({
-                          icon: 'error',
-                          title: 'Oops...',
-                          text: 'Something went wrong! ' + xhr.status + ' ' + thrownError,
-                          // footer: '<a href="">Why do I have this issue?</a>'
-                      })
-                  }
-              })
-          }
-      })
-
-
+    //               data: form,
+    //               success: function(data) {
+    //                   console.log(data);
+    //                   Swal.fire(
+    //                   'Updated!',
+    //                   'Your Record Was Updated Succesfully.',
+    //                   'success'
+    //                   ).then(() => {
+    //                     location.reload();
+    //                   })
+    //                   // clearTable();
+    //                   // getTable();
+    //                   // checkForm();
+    //               },
+    //               error : function (xhr, ajaxOptions, thrownError) {
+    //                   Swal.fire({
+    //                       icon: 'error',
+    //                       title: 'Oops...',
+    //                       text: 'Something went wrong! ' + xhr.status + ' ' + thrownError,
+    //                       // footer: '<a href="">Why do I have this issue?</a>'
+    //                   })
+    //               }
+    //           })
+    //       }
+    //   })
 
         })
     })
