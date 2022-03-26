016909e6209d17ff5864418eec4242f6afc55904
Kumudperera <nethmalperera54@gmail.com>
2022-03-27 00:17:18 +0530

    Supervisor modified

--- app/controllers/Supervisor.php
+++ app/controllers/Supervisor.php
@@ -151,7 +151,7 @@ class Supervisor extends Controller
                     $monthName = date('F', strtotime("2001-$monthNum-1"));
                     echo '<div class="manage-request-row">
                             <div class="manage-request-cell" data-title="Previous Request Date"><b>' . $lastRequests[$i]['request_date'] . '</b></div>
-                            <div class="manage-request-cell" data-title="Monthly Tea Amount(kg)">' . $this->model->getMonthTeaWeight($month, $_POST['landowner_id']) . '</div>
+                            <div class="manage-request-cell" data-title="Monthly Tea Amount(kg)">' . $this->model->getMonthTeaWeight($monthNum, $_POST['landowner_id']) . '</div>
                         </div>';
                 }
                 echo '</div>';
