59daeb6fcbb6e808cb500e0b110124399aecafee
Kumudperera <nethmalperera54@gmail.com>
2022-03-27 16:52:51 +0530

    Supervisor modified

--- vendors/js/landowner/Update_Tea_Availabilityjs.php
+++ vendors/js/landowner/Update_Tea_Availabilityjs.php
@@ -2,47 +2,75 @@
     $(document).ready(function() {
         $('#update_availability').click(function(event) {
             var form = $('#update_availability_form').serializeArray();
-            // console.log($(this).val());
+            var numberOfContainers = form[0]['value'];
+            var errorMessage = "";
 
-            Swal.fire({
-                title: 'Are you sure?',
-                text: 'You won\'t be able to revert this!',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#01830c',
-                cancelButtonColor: '#FF2400',
-                confirmButtonText: 'Yes, Update it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    $('#update_availability_form').trigger('reset');
-                    $.ajax({
-                        type: 'POST',
-                        url: '<?php echo URL ?>landowner/Update_Tea_Availability',
-                        cache: false,
-                        data: form,
-                        success: function(data) {
-                            Swal.fire({
-                                icon: 'success',
-                                title: 'Updated !',
-                                text: 'Your file has been updated.',
-                                confirmButtonColor: '#01830c'
-                            }).then(() => {
-                                location.reload();
-                            })
-                            // console.log(data);
-                        },
-                        error: function(xhr, ajaxOptions, thrownError) {
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Oops...',
-                                text: 'Something went wrong! ' + xhr.status + ' ' + thrownError,
-                            })
-                        }
-                    })
-                } else {
-                    $('#update_availability_form').trigger('reset');
-                }
+            function showError(erroMessage, errorField) {
+                $('#update_availability_form').trigger('reset');
+                $(errorField).html(erroMessage);
+            }
+
+            function removeError(errorField) {
+                errorMessage = "";
+                $(errorField).html("");
+            }
+
+            function containsSpecialChars(str) {
+                const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+                return specialChars.test(str);
+            }
+            $("#priceid").keydown(function() {
+                removeError("#container_error");
             })
+            if (numberOfContainers < 0) {
+                errorMessage = "*Can't insert minus Value !";
+                showError(errorMessage, "#container_error");
+            } else if (isNaN(numberOfContainers)) {
+                errorMessage = "*Can't contain characters !";
+                showError(errorMessage, "#container_error");
+            } else {
+                removeError("#container_error");
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: 'You won\'t be able to revert this!',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#01830c',
+                    cancelButtonColor: '#FF2400',
+                    confirmButtonText: 'Yes, Update it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $('#update_availability_form').trigger('reset');
+                        $.ajax({
+                            type: 'POST',
+                            url: '<?php echo URL ?>landowner/Update_Tea_Availability',
+                            cache: false,
+                            data: form,
+                            success: function(data) {
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Updated !',
+                                    text: 'Your file has been updated.',
+                                    confirmButtonColor: '#01830c'
+                                }).then(() => {
+                                    location.reload();
+                                })
+                                // console.log(data);
+                            },
+                            error: function(xhr, ajaxOptions, thrownError) {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Oops...',
+                                    text: 'Something went wrong! ' + xhr.status + ' ' + thrownError,
+                                })
+                            }
+                        })
+                    } else {
+                        $('#update_availability_form').trigger('reset');
+                    }
+                })
+            }
+
         })
     })
 </script>
